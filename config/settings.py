c96cae03759c0efd1a7ee8c1ae17db1e3e3daa2b
Elon-陈龙 <3368752532@qq.com>
2021-12-04 17:30:41 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-adpzsk-wv+8myx-b@j449a)qcur!a&#q)9&7g5gjut=nq4&+6-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
